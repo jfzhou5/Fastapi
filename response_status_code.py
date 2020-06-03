b749c68833a4aa34e9298d710b085aa16d73b29f
xyz25 <1241330802@qq.com>
2020-06-03 13:51:54 +0800

    'response_status_code'

--- response_status_code.py
+++ response_status_code.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from starlette.status import HTTP_201_CREATED  # 从响应码库中获取响应码常量
 
 app = FastAPI()
 
+
+# @app.post('/items/', status_code=201)  # 201：created
+@app.post('/items/', status_code=HTTP_201_CREATED)  # HTTP_201_CREATED=201
+async def create_item(name: str):
+    return {'item_name': name}
